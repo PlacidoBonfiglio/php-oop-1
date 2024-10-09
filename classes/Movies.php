cf0d2ce67bc2ecaab456ea83f250af235d10d579
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-09 16:46:39 +0200

    added genre property

--- classes/Movies.php
+++ classes/Movies.php
@@ -6,31 +6,34 @@ class Movie {
     // proprietà che servono per assegnare un valore 
     public $name;
     public $length;
+    public $genre;
     public $vote;
 
     // creo una funzione che assegna agli argomenti name, length e voto
     function __construct(
         string $_name,
         string $_length,
+        string $_genre,
         int $_vote,
     ){
         $this->name ="$_name";
         $this->length = "$_length";
+        $this->genre = "$_genre";
         $this->vote = $_vote;
     }
 }
 
 // Creo un oggetto di tipo "Movie" con propietà name, length e voto
-$atlantis = new Movie("Atlantis", "1h 20m", 5);
+$theRing = new Movie("The ring", "1h 55m", "Horror", 7);
 
-$eldorado = new Movie("El Dorado", "1h 29m", 7);
+$furiosa = new Movie("Furiosa: A Mad Max Saga", "2h 28m", "Action", 7);
 
-$ilPianetaDelTesoro = new Movie("Il Pianeta del tesoro", "1h 35m", 9);
+$ilPianetaDelTesoro = new Movie("Il Pianeta del tesoro", "1h 35m", "Adventure", 8);
 
-$LeFollieDellImperatore = new Movie("Le Follie dell'Imperatore", "1h 18m", 9);
+$LeFollieDellImperatore = new Movie("Le Follie dell'Imperatore", "1h 18m", "Adventure", 8);
 
-var_dump($atlantis);
-var_dump($eldorado);
+var_dump($theRing);
+var_dump($furiosa);
 var_dump($ilPianetaDelTesoro);
 var_dump($LeFollieDellImperatore);
 
